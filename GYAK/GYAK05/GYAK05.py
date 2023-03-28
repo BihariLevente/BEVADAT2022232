8eeb8b4fa9600eaf5c09dd1c0d5803ba71f56966
BihariLevente <leventebihari3@gmail.com>
2023-03-29 00:17:14 +0200

    gyak05 corrected

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -5,24 +5,20 @@ from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
 class KNNClassifier:
-    def __init__ (self, k : int, test_split_ratio : float):
+    def __init__ (self, k : int, test_split_ratio : float) -> None:
         self.k = k
         self.test_split_ratio = test_split_ratio
+
     
     @staticmethod
-    def load_csv(csv_path:str) -> Tuple[np.ndarray,np.ndarray]:
+    def load_csv(self, csv_path:str) -> Tuple[np.ndarray,np.ndarray]:
         np.random.seed(42)
         dataset = np.genfromtxt(csv_path,delimiter=',')
         np.random.shuffle(dataset,)
         x,y = dataset[:,:4],dataset[:,-1]
-        x[np.isnan(x)] = 3.5
-        y = np.delete(y,np.where(x < 0.0)[0],axis=0)
-        y = np.delete(y,np.where(x > 13.0)[0],axis=0)
-        x = np.delete(x,np.where(x < 0.0)[0],axis=0)
-        x = np.delete(x,np.where(x > 13.0)[0],axis=0)
         return x,y
     
-    def train_test_split(features:np.ndarray, labels:np.ndarray):
+    def train_test_split(self, features:np.ndarray, labels:np.ndarray):
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
 
@@ -33,10 +29,10 @@ class KNNClassifier:
         self.x_test = features[train_size:train_size+test_size,:]
         self.y_test = labels[train_size:train_size + test_size]
     
-    def euclidean(element_of_x:np.ndarray) -> np.ndarray:
+    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
         return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
 
-    def predict(x_test:np.ndarray,k:int) -> np.ndarray:
+    def predict(self, x_test:np.ndarray,k:int) -> np.ndarray:
         labels_pred = []
         for x_test_element in x_test:
             distances = self.euclidean(self.x_train,x_test_element)
@@ -45,11 +41,11 @@ class KNNClassifier:
             labels_pred.append(label_pred)
         self.y_preds = np.array(labels_pred,dtype=np.int32)
     
-    def accuracy() -> float:
+    def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
-    def confusion_matrix():
+    def confusion_matrix(self):
         conf_matrix = confusion_matrix(self.y_test,self.y_preds)
         sns.heatmap(conf_matrix,annot=True) 
 
