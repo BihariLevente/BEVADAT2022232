373c841ed6d29168bc1556953a6e083be21d550a
BihariLevente <leventebihari3@gmail.com>
2023-03-29 21:15:59 +0200

    hw05

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import seaborn as sns
 from typing import Tuple
@@ -8,17 +9,16 @@ class KNNClassifier:
     def __init__ (self, k : int, test_split_ratio : float) -> None:
         self.k = k
         self.test_split_ratio = test_split_ratio
-
     
     @staticmethod
-    def load_csv(self, csv_path:str) -> Tuple[np.ndarray,np.ndarray]:
-        np.random.seed(42)
-        dataset = np.genfromtxt(csv_path,delimiter=',')
-        np.random.shuffle(dataset,)
-        x,y = dataset[:,:4],dataset[:,-1]
+    def load_csv(self, csv_path:str) -> Tuple[pd.DataFrame , pd.DataFrame]:
+        dataset = pd.read_csv(csv_path, delimiter=',')
+        dataset = dataset.sample(frac=1, random_state=42).reset_index(drop=True)
+        x,y = dataset.iloc[:,:4], dataset.iloc[:,-1]
         return x,y
-    
-    def train_test_split(self, features:np.ndarray, labels:np.ndarray):
+
+    def train_test_split(self, features: pd.DataFrame, labels: pd.DataFrame) -> None:
+        
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
 
@@ -29,17 +29,17 @@ class KNNClassifier:
         self.x_test = features[train_size:train_size+test_size,:]
         self.y_test = labels[train_size:train_size + test_size]
     
-    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
+    def euclidean(self, element_of_x: pd.DataFrame) -> pd.DataFrame:
+        return pd.sqrt(pd.sum((self.x_train - element_of_x)**2,axis=1))
 
-    def predict(self, x_test:np.ndarray) -> np.ndarray:
+    def predict(self, x_test:pd.DataFrame) -> pd.DataFrame:
         labels_pred = []
         for x_test_element in x_test:
             distances = self.euclidean(self.x_train,x_test_element)
-            distances = np.array(sorted(zip(distances, self.y_train)))
+            distances = pd.DataFrame(sorted(zip(distances,self.y_train)))
             label_pred = mode(distances[:self.k,1],keepdims=False).mode
             labels_pred.append(label_pred)
-        self.y_preds = np.array(labels_pred,dtype=np.int32)
+        self.y_preds = pd.DataFrame(labels_pred,dtype=pd.int32)
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
