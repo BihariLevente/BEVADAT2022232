fd2a6374d72ce008078fba4bc474c24c01178101
BihariLevente <leventebihari3@gmail.com>
2023-04-01 22:01:00 +0200

    hazi05 corrected

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -11,33 +11,32 @@ class KNNClassifier:
         self.test_split_ratio = test_split_ratio
     
     @staticmethod
-    def load_csv(csv_path:str) -> Tuple[pd.DataFrame , pd.DataFrame]:
+    def load_csv(csv_path:str) -> Tuple[pd.DataFrame , pd.Series]:
         dataset = pd.read_csv(csv_path, delimiter=',')
         dataset = dataset.sample(frac=1, random_state=42).reset_index(drop=True)
-        x,y = dataset.iloc[:,:4], dataset.iloc[:,-1]
+        x,y = dataset.iloc[:,:-1], dataset.iloc[:,-1]
         return x,y
 
-    def train_test_split(self, features: pd.DataFrame, labels: pd.DataFrame) -> None:
-        
+    def train_test_split(self, features: pd.DataFrame, labels: pd.Series):
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
 
         assert len(features) == test_size + train_size, "Size mismatch!"
 
-        self.x_train = features[:train_size,:]
-        self.y_train = labels[:train_size]
-        self.x_test = features[train_size:train_size+test_size,:]
-        self.y_test = labels[train_size:train_size + test_size]
+        self.x_train = features.iloc[:train_size,:].reset_index(drop=True)
+        self.y_train = labels.iloc[:train_size].reset_index(drop=True)
+        self.x_test = features.iloc[train_size:train_size+test_size,:].reset_index(drop=True)
+        self.y_test = labels.iloc[train_size:train_size + test_size].reset_index(drop=True)
     
-    def euclidean(self, element_of_x: pd.DataFrame) -> pd.DataFrame:
-        return pd.sqrt(pd.sum((self.x_train - element_of_x)**2,axis=1))
+    def euclidean(self, element_of_x: pd.Series) -> pd.Series:
+        return ((self.x_train - element_of_x)**2).sum(axis=1).apply(np.sqrt)
 
-    def predict(self, x_test:pd.DataFrame) -> pd.DataFrame:
+    def predict(self, x_test: pd.DataFrame):
         labels_pred = []
-        for x_test_element in x_test:
-            distances = self.euclidean(self.x_train,x_test_element)
-            distances = pd.DataFrame(sorted(zip(distances,self.y_train)))
-            label_pred = mode(distances[:self.k,1],keepdims=False).mode
+        for x_test_element in x_test.values:
+            distances = self.euclidean(x_test_element)
+            distances_df = pd.DataFrame(zip(distances,self.y_train)).sort_values(by='distance')
+            label_pred = mode(distances_df[:self.k,1],keepdims=False).mode
             labels_pred.append(label_pred)
         self.y_preds = pd.DataFrame(labels_pred,dtype=pd.int32)
     
