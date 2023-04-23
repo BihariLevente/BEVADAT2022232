18dea69d039ab2a2f86c1d4f10587ee95fcd8fe0
BihariLevente <leventebihari3@gmail.com>
2023-04-23 22:40:08 +0200

    HAZI08.py added

---
+++ HAZI/HAZI08/HAZI08.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn 
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error
+
+def load_iris_data() -> sklearn.utils.Bunch: 
+    return load_iris()
+
+def check_data(iris) -> pd.core.frame.DataFrame:
+    iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    return iris_df.head(5)
+
+def linear_train_data(iris) -> (np.ndarray, np.ndarray):
+    iris_database = iris
+    X = np.array(iris_database.data[:,1:4])
+    y = np.array(iris_database.data[:,0])
+    return (X,y)
+
+def logistic_train_data(iris) -> (np.ndarray, np.ndarray):
+    iris_database = iris
+    mask = np.where((iris_database.target == 0) | (iris_database.target == 1))
+    X = np.array(iris_database.data[mask])
+    y = np.array(iris_database.target[mask])
+    return (X,y)
+
+def split_data(X, y) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def train_linear_regression(X_train, y_train) -> LinearRegression:
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def train_logistic_regression(X_train, y_train) -> LogisticRegression: #error with "sklearn.linear_model._base.LogisticRegression"
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def predict(model , X_test) -> np.ndarray:
+    return model.predict(X_test)
